a5e87fc220028c1466290fd902f58c2036eeeaf7
ESRAILHAQUE <esrailbblhs@gmail.com>
2026-03-20 16:47:47 +0600

    add:features

--- prisma/migrations/20260210192232_add_isverified_isblock_jwt_refresh_secret/migration.sql
+++ prisma/migrations/20260210192232_add_isverified_isblock_jwt_refresh_secret/migration.sql
@@ -4,6 +4,9 @@ CREATE TYPE "GraduationStatus" AS ENUM ('studying', 'graduated', 'dropped');
 -- CreateEnum
 CREATE TYPE "AccountType" AS ENUM ('student', 'alumni', 'teacher');
 
+
+
+
 -- CreateEnum
 CREATE TYPE "PaymentStatus" AS ENUM ('paid', 'partial', 'unpaid');
 
